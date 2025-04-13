b19ab4f1212f986a6e8dfc9fc209c8bddf959662
Minhal Aslam <167016498+MinhalAslam@users.noreply.github.com>
2025-04-13 20:10:41 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+# 1 Integer(int)
+# Whole numbers (positive or negative), no decimal.
+x = 10
+y = -5
+print(type(x))  # Output: <class 'int'>
+
+# 2 Flot(float)
+# Numbers with decimal points.
+price = 9.99
+height = -5.5
+print(type(price))  # Output: <class 'float'>
+
+# 3 String(str)
+# Text — anything inside quotes ' ' or " ".
+name = "Minhal"
+greeting = 'Good Morning!'
+print(type(name))  # Output: <class 'str'>
+
+# 4 Boolean(bool)
+# Only True or False values
+is_raining = True
+is_humid = False
+print(type(is_raining))  # Output: <class 'bool'>
+
+# 5 List(list)
+# A collection of items, written in square brackets []
+fruits = ["apple", "banana", "cherry" , "mango"]
+print(type(fruits))  # Output: <class 'list'>
+
+# 6 Tuple(tuple)
+# Like a list, but unchangeable (immutable), written in parentheses ()
+colors = ("red", "green", "blue" , "brown")
+print(type(colors))  # Output: <class 'tuple'>
+
+# 7 Dictionary(dict)
+# Stores data in key-value pairs, inside curly braces {}
+person = {"name": "Adil", "age": 30}
+print(type(person))  # Output: <class 'dict'>
+
+# 8 Set(set)
+# A collection of unique items, written in curly braces {} (no duplicates!)
+unique_numbers = {1, 2, 3, 2, 1}
+print(unique_numbers)  # Output: {1, 2, 3}
+print(type(unique_numbers))  # Output: <class 'set'>
+
+
+
+
+
