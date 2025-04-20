c974cc008fde1b112eb8e8b8cf20e4da818bc2f1
Minhal Aslam <167016498+MinhalAslam@users.noreply.github.com>
2025-04-20 14:42:02 +0500

    Add files via upload

---
+++ assignment4/function.py
@@ -0,0 +1,8 @@
+# function.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
---
+++ assignment4/main.py
@@ -0,0 +1,13 @@
+# main.py
+
+import function
+
+def main():
+    x = 10
+    y = 5
+
+    print("Adding:", function.add(x, y))
+    print("Subtracting:", function.subtract(x, y))
+
+if __name__ == "__main__":
+    main()
